e63807a0b48684caa11904cd4c92ee11cb16a1a7
threldor <threldor@gmail.com>
2012-10-05 18:01:03 +0000

    worling

--- wof.py
+++ wof.py
@@ -19,65 +19,80 @@
 # after defined time the given mp3 is played for that section
 
 from time import sleep
-import os
+import os, subprocess
 import RPi.GPIO as GPIO
 
 # define song list in array (could make it smart and take the first 12 songs)
 def setupSongList(songs):
-	song1 = '001-FeelSoGood.mp3'
-	song2 = '002-LoveFoolosophy.mp3'
-	song3 = '003-ReadyForTheFloor.mp3'
-	song4 = '004-AndIWasABoyFromSchool.mp3'
-	song5 = '005-DANCE.mp3'
-	song6 = '006-Newjack.mp3'
-	songs.append(song1)
-	songs.append(song2)
-	songs.append(song3)
-	songs.append(song4)
-	songs.append(song5)
-	songs.append(song6)
+    song1 = '001.mp3'
+    song2 = '002.mp3'
+    song3 = '003.mp3'
+    song4 = '004.mp3'
+    song5 = '005.mp3'
+    song6 = '006.mp3'
+    song7 = '007.mp3'
+    song8 = '008.mp3'
+    song9 = '009.mp3'
+    song10 = '010.mp3'
+    song11 = '011.mp3'
+    song12 = '012.mp3'
+    songs.append(song1)
+    songs.append(song2)
+    songs.append(song3)
+    songs.append(song4)
+    songs.append(song5)
+    songs.append(song6)
+    songs.append(song7)
+    songs.append(song8)
+    songs.append(song9)
+    songs.append(song10)
+    songs.append(song11)
+    songs.append(song12)
 
 # define wait time in seconds before playing mp3
-waitTime = 3
+waitTime = 6
 
 # setup variables
 debounce = []
 position = -1
-songs[]
+songs = []
 sectionChanged = False
-coupler1 = 17
+coupler1 = 22
 coupler2 = 23
 coupler3 = 24
 coupler4 = 25
+print "done initial values"
 
 # play the mp3
 def playMP3(songName):
-	# so we need to stop, clear, add then play
-	stopMP3()
-	os.system("C:\\Portable\\mpc\\mpc.exe add " + songName)
-	cmd = subprocess.Popen("C:\\Portable\\mpc\\mpc.exe play",shell=True, stdout=subprocess.PIPE)
-	print "Section : " + str(section) + " playing.....  " + cmd.stdout.readline()
+    # so we need to stop, clear, add then play
+    stopMP3()
+    os.system("sudo mpc add " + songName)
+    cmd = subprocess.Popen("sudo mpc play",shell=True, stdout=subprocess.PIPE)
+    print "Section : " + songName + " playing.....  " + cmd.stdout.readline()
 
 # stop the mp3
 def stopMP3():
-	os.system("C:\\Portable\\mpc\\mpc.exe stop")
-	os.system("C:\\Portable\\mpc\\mpc.exe clear")
+    os.system("sudo mpc stop")
+    os.system("sudo mpc clear")
+    print "stop"
 
 # setup sound and mpd
 def setupMPD():
-	os.system("sudo modprobe snd_bcm2835")
-	os.system("sudo amixer cset numid=3 1")
-	os.system("sudo mpd")
-	os.system("sudo mpc")
-	os.system("sudo mpc clear")
-	os.system("sudo mpc volume 25")
+    #os.system("sudo modprobe snd_bcm2835")
+    #os.system("sudo amixer cset numid=3 1")
+    #os.system("sudo mpd")
+    os.system("sudo mpc")
+    os.system("sudo mpc clear")
+    #os.system("sudo mpc volume 25")
+    print "setupMPD"
 
 # create the debounce array with length waitTime
 def setupDebounce(waitTime, debounce):
-	var = waitTime
-	while var > 0:
-		debounce.append(-1)
-		var -= 1
+    var = waitTime
+    while var > 0:
+        debounce.append(-1)
+        var -= 1
 
 # update the debounce array with new position
 def updateDebounce(debounce, position):
@@ -94,45 +109,45 @@ def checkPosition(debounce):
         if pos != deb:
             return -1
     return pos
-	
+    
 # check inputs 
 # returns -1 if invalid otherwise returns number from 0 to 11
 def checkInputs():
-	# greycode goes
-	# 0123456789AB  segment number where A is 10 and B is 11
-	# 000000001111  inside  8
-	# 000011111100          4
-	# 001111000110          2
-	# 011001100000  oustide 1
-	# value in binary
-	# 013267541118
-	#         240
-	
-	# map the above into array with -1 where invalid
-	pos = [0,1,3,2,7,6,4,5,11,-1,10,-1,8,-1,9,-1]
-	
-	greyValue = 0
-	
-	# get grey value
-	if ( GPIO.input(coupler1) == False ):
-		greyValue += 1
-	if ( GPIO.input(coupler2) == False ):
-		greyValue += 2
-	if ( GPIO.input(coupler3) == False ):
-		greyValue += 4
-	if ( GPIO.input(coupler4)== False ):
-		greyValue += 8
-	
-	return pos[greyValue]
+    # greycode goes
+    # 0123456789AB  segment number where A is 10 and B is 11
+    # 000000001111  bottom  8
+    # 000011111100          4
+    # 001111000110          2
+    # 011001100000  top     1
+    # value in binary
+    # 013267541118
+    #         240
+    
+    # map the above into array with -1 where invalid
+    pos = [0,1,3,2,7,6,4,5,11,-1,10,-1,8,-1,9,-1]
+    
+    greyValue = 0
+    
+    # get grey value
+    if ( GPIO.input(22) == True ):
+        greyValue += 1
+    if ( GPIO.input(23) == True ):
+        greyValue += 2
+    if ( GPIO.input(24) == True ):
+        greyValue += 4
+    if ( GPIO.input(25)== True ):
+        greyValue += 8
+
+    return pos[greyValue]
 
 # to change to BCM GPIO numbering
 GPIO.setmode(GPIO.BCM) 
 
 # setup pin directions
-GPIO.setup(coupler1, GPIO.IN)
-GPIO.setup(coupler2, GPIO.IN)
-GPIO.setup(coupler3, GPIO.IN)
-GPIO.setup(coupler4, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
 
 #setup pullup resistors on all inputs
 #GPIO.setup(coupler1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -147,33 +162,42 @@ setupDebounce(waitTime, debounce)
 # setup sound and MPD
 setupMPD()
 
+playingMusic = False
+
+print "starting loop....."
+
 while True:
-	# check the inputs
-	positionNow = checkInputs()
-	
-	# update the debounce array
-	updateDebounce(debounce, positionNow)
-	
-	# check the actual position
-	position = checkPosition(debounce)
-	
-	# update allow playing only if the wheel has changed sections from where it stopped last
-	# this prevents the same song playing over and over again
-	# also stop any currently playing songs
-	if position < 0:
-		sectionChanged = True
-		stopMP3()
-	
-	# play mp3 if position is valid
-	if (position >= 0 and sectionChanged):
-		sectionChanged = False
-		
-		# play the required song
-		playMP3(songs[position])
-		# possibly add control to activate the solenoid to prevent movement
-		# would then need to sleep() for the time the mp3 plays
-		# then release the solenoid and continue
-	
-	sleep(1);
+    # check the inputs
+    positionNow = checkInputs()
+    
+    # update the debounce array
+    updateDebounce(debounce, positionNow)
+    
+    # check the actual position
+    # print "get actual position..."
+    position = checkPosition(debounce)
+    print "position is: " + str(position)
+    
+    # update allow playing only if the wheel has changed sections from where it stopped last
+    # this prevents the same song playing over and over again
+    # also stop any currently playing songs
+    if position < 0:
+        sectionChanged = True
+        if playingMusic:
+                        stopMP3()
+                        playingMusic = False
+    
+    # play mp3 if position is valid
+    if (position >= 0 and sectionChanged):
+        sectionChanged = False
+        
+        # play the required song
+        playMP3(songs[position])
+        playingMusic = True
+        # possibly add control to activate the solenoid to prevent movement
+        # would then need to sleep() for the time the mp3 plays
+        # then release the solenoid and continue
+    
+    sleep(0.5);
 
 print "done"
